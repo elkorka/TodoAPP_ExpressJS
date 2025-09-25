8f15947b1bcc7f576289e274bb70ed49351727c7
korka <emkdiallo@gmail.com>
2025-09-25 18:50:12 +0000

    7commit

--- prisma/migrations/20250925035428_init2/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX `Subtask_taskId_fkey` ON `subtask`;
-
--- AddForeignKey
-ALTER TABLE `Subtask` ADD CONSTRAINT `Subtask_taskId_fkey` FOREIGN KEY (`taskId`) REFERENCES `Task`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
